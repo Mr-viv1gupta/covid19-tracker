256a0b2c187c596434aa20362aef64246b1f9592
Mr-viv1gupta <viv1gupta9872@hotmail.com>
2020-08-02 00:10:30 +0530

    Global Summary Data, covid19Api country based

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
 const pug = require('pug');
+const axios = require('axios');
 
 // Home page route.
 router.get('/', (req, res) => {
-    res.render('home');
-    // res.send('Hello World. Learn <a href="/about">about this app</a>');
-});
+    const apiUrl = "https://api.covid19api.com/summary";
+    const countries = axios.get(apiUrl).then((response) => {
+        res.render("home", {
+          appName: "My COVID-19 Tracker",
+          pageName: "COVID-19 Cases",
+          data: response.data,
+        });
+      })
+      .catch(function (err) {
+        return console.error(err);
+      });
+  });
 
 // About page route
 router.get('/about', (req, res) => {
